4738eb1c19431971234715b7f27470e661efea8c
KoreanCodingMachine <pon03078@naver.com>
2023-02-28 04:24:27 +0900

    뒤에 있는 큰 수 찾기 런타임 에러

--- 프로그래머스 lv2/뒤에 있는 큰 수 찾기.js
+++ 프로그래머스 lv2/뒤에 있는 큰 수 찾기.js
@@ -6,14 +6,57 @@
 // 접근 방법
 // 배열을 정렬해서  가장 큰 수를 저장할 변수를 하나 만들어서 그 수는 -1로 대체함
 
+// 런타임 에러 -> 시간복잡도 초과
+// function solution(numbers) {
+//   var answer = [];
+
+//   let max = Math.max(...numbers); // 9
+
+//   for (let i = 0; i < numbers.length; i++) {
+//     for (let j = i; j < numbers.length; j++) {
+//       if (numbers[i] === max) {
+//         answer.push(-1);
+//         break;
+//       }
+
+//       if (numbers[i] < numbers[j]) {
+//         answer.push(numbers[j]);
+//         break;
+//       }
+
+//       if (j === numbers.length - 1) {
+//         answer.push(-1);
+//         break;
+//       }
+//     }
+//   }
+//   return answer;
+// }
+
+// stack
+
 function solution(numbers) {
-  var answer = [];
+  const answer = Array(numbers.length).fill(-1);
+  const stack = [];
 
-  let max = Math.max(...numbers); // 9
-  let sort = numbers.sort((a, b) => a - b); // [-1,2,3,5,6,9]
-
+  for (let i = 0; i < numbers.length; i++) {
+    while (stack.length && numbers[stack.at(-1)] < numbers[i]) {
+      answer[stack.pop()] = numbers[i];
+    }
+    stack.push(i);
+  }
   return answer;
 }
 
+// stack -> [0 , 5 ,  ]
+
 console.log(solution([2, 3, 3, 5]));
 console.log(solution([9, 1, 5, 3, 6, 2]));
+
+// Array.prototype.at 메서드 -> 매개변수로 인덱스 번호 -> 해당하는 인덱스의 값을 리턴함
+// 음수 매개변수로 뒤에서부터 접근할 때 유용함
+// const array = [1, 2, 3, 4, 5];
+// console.log(array.at(0)); // 1
+// console.log(array.at(3)); // 4
+// console.log(array.at(-1)); // 5
+// console.log(array.at(-2)); // 4
